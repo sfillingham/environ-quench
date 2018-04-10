b0fb912c510d5cc13168eb5c81e4db0625e748b9
Sean Fillingham <sfilling@uci.edu>
2018-04-10 00:09:04 -0700

    update select_halos

--- select_halos.py
+++ select_halos.py
@@ -72,3 +72,74 @@ def centrals(userpath, halofile, mass_range=[1.e12, 1.e15], chunk=100):
     centralgals.to_csv(userpath+haloname+'_centralhalos.csv')
 
     return centralgals
+
+
+def satellites(userpath, halofile, hostfile, mass_range=[1.e10, 1.e13], dist=1.0, chunk=100):
+    """This function will select all satellites in the simulation
+    based on the mass range specified and proximity to the host.
+    This function assumes the halocatalogs are sorted by virial mass.
+
+    Parameters
+    ----------
+    userpath : string
+        the directory path that points to where the outputs will be stored
+    halofile : string
+        the input halo catalog file
+    hostfile : string
+        the input list of centrals to be used as the host locations
+    mass_range : tuple
+        min_mass, max_mass
+    dist : float
+        The maximum distance a halo can reside to be considered a satellite.
+    chunk : float
+        The number of lines to read in at a time.
+
+    Returns
+    -------
+    satellites : pd.DataFrame
+        A pandas.DataFrame that contains all of the input columns for the 
+        selected satellite halos.
+
+    """
+    assert mass_range[1] > mass_range[0]
+    i = 0
+
+    #grab the halo catalog
+    rows = [1, 57]
+    read_halo = grab.reader(halofile, skiprows=rows)
+    halo_name = halofile.split('/')[-1]
+    halo_sname = halo_name.split('.')
+    haloname = halo_sname[0]+'.'+halo_sname[1]
+
+    #grab central galaxy catalog
+    centrals = pd.read_csv(userpath+haloname+'_centralhalos.csv')
+
+    #chunk size to test against
+    size=chunk
+
+    while chunk == size:
+
+        datachunk = read_halo.get_chunk(chunk)
+        keys = datachunk.keys()
+        
+        selectcentral = np.where(np.logical_and(datachunk['mvir(10)'] > mass_range[0],
+                                                    datachunk['mvir(10)'] < mass_range[1]))[0]
+
+        centralchunk = datachunk.iloc[selectcentral]
+        
+        if i == 0:
+            centralgals = centralchunk
+        else:
+            centralgals.append(centralchunk)
+        
+        i += 1
+
+        print(i)
+        chunk = len(datachunk)
+
+    halo_sname = halo_name.split('.')
+    haloname = halo_sname[0]+'.'+halo_sname[1]
+    print(haloname)
+    centralgals.to_csv(userpath+haloname+'_satellitehalos.csv')
+
+    return centralgals
