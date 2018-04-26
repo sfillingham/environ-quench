b62cf7378d268a23c3088840230a62366488ab54
Sean Fillingham <sfilling@uci.edu>
2018-04-26 12:07:07 -0700

    updating select_mainbranch

--- select_mainbranch.py
+++ select_mainbranch.py
@@ -32,6 +32,45 @@ def tree_chunk(halochunk, location, forest):
 
     return treechunk
 
+
+def tree_evolution(halocat, locationfile, forestfile, keylist=['x', 'y', 'z']):
+    """Given a list of halos in the halocat, this will return the evolution of the parameters
+    listed in the keylist.
+
+    Parameters
+    ----------
+    halocat : pd.DataFrame
+    locationfile : string
+        The directory and file that contains the mapping between the
+        TreeRootID and the merger tree file
+    forestfile : string
+        Same as locationfile except for the forest
+    keylist : tuple
+        A list of the DataFrame.keys() to be part of the ouput
+
+    Returns
+    -------
+    A pd.DataFrame that contains the temporal evolution for every paramter listed
+    in the keylist.
+
+    """
+    location = pd.read_table(locationfile)
+    forest = pd.read_table(forestfile)
+    
+    haloid = halocat['Tree_root_ID(29)']
+    treename = location.iloc[np.where(location['#TreeRootID'] == haloid)[0]]
+    uniquetree = np.unique(treename)
+    rows = np.linspace(1, 47, 47)
+
+    for i in range(len(uniquetree)):
+
+        filename = '/Users/tmc/bolshoi/bolshoisims/trees/'+treename
+        tree = pd.read_table(filename, delim_whitespace=True, skiprows=rows)
+        
+    
+
+    
+
 #Identify host halos, i.e. above 1.e13
 #Identify subhalos for every host halo
 #Match all halos to a tree file via location.dat
